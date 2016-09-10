901713377addfe9fed3cd8bc7c6c7f47184313fc
Nestor <nmendoza@arawato.com.ve>
2016-09-10 09:36:23 -0400

    Nueva version material. Colores de prueba

--- src/themeConfig.js
+++ src/themeConfig.js
@@ -1,5 +1,9 @@
 /** @ngInject */
 angular.module('app').config(['$mdThemingProvider', '$mdIconProvider', themeConfig]);
 function themeConfig($mdThemingProvider, $mdIconProvider) {
+  $mdThemingProvider.theme('default')
+          .primaryPalette('deeporange')
+          .accentPalette('light-green')
+          .warnPalette("teal");
 }
 
