8e1542b73fe8b4bfee2ac8c98900771f53846ea4
User <User@DESKTOP-F9MCFV5>
2016-09-17 10:57:04 -0400

    diseño de agregar categorias y sus subcategorias casi "Terminado"

--- src/app/restaurante/tab-general.js
+++ src/app/restaurante/tab-general.js
@@ -5,38 +5,59 @@ angular
             controller: tabGeneralCtrl,
             bindings: {
                 config: '=',
-                numero: '='
+                categoria: '='
             }
         });
 
 function tabGeneralCtrl($q, $timeout, $log) {
 
     var $ctrl = this;
-    $ctrl.config={};
-    $ctrl.config.beAble=[{able:false, numero:1},{able:true, numero:2},{able:true, numero:3},{able:true, numero:4}];
-    $ctrl.config.selectedIndex=0;
+    $ctrl.config = {};
+    $ctrl.config.beAble = [{able: false, numero: 1}, {able: true, numero: 2}, {able: true, numero: 3}, {able: true, numero: 4}];
+    $ctrl.config.selectedIndex = 0;
     $ctrl.toppings = [
-        {name: 'Pepperoni', wanted: true},
-        {name: 'Sausage', wanted: false},
-        {name: 'Black Olives', wanted: true},
-        {name: 'Green Peppers', wanted: false}
+        {name: 'Comida Para niños', wanted: true, porcentaje: 80},
+        {name: 'Bebidas', wanted: true, porcentaje: 70},
+        {name: 'Pastas', wanted: true, porcentaje: 85},
+        {name: 'Granos', wanted: false, porcentaje: 20},
+        {name: 'Sausage', wanted: false, porcentaje: 30},
+        {name: 'Parrillas', wanted: true, porcentaje: 90},
+        {name: 'Black Olives', wanted: true, porcentaje: 75},
+        {name: 'Desayunos', wanted: false, porcentaje: 20},
+        {name: 'Almuerzos', wanted: false, porcentaje: 45},
+        {name: 'Cena', wanted: true, porcentaje: 80},
+        {name: 'Green Peppers', wanted: false, porcentaje: 20}
     ];
     $ctrl.hola = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
-    $ctrl.goTo = function (numb) {
-        numb=numb+2;
+    $ctrl.goTo = function (numb, item) {
+        numb = numb + 2;
+        $ctrl.categoria=item.name;
+//        localStorage.getItem("pase.fit.storage-roles");
+        localStorage.setItem("vamosacomer.storage-item", JSON.stringify(item));
         console.log(numb);
         for (var i = 0; i < $ctrl.config.beAble.length; i++) {
-            if ($ctrl.config.beAble[i].numero<=numb) {
-                $ctrl.config.beAble[i].able=false;
-            }else{
-                $ctrl.config.beAble[i].able=true;
+            if ($ctrl.config.beAble[i].numero <= numb) {
+                $ctrl.config.beAble[i].able = false;
+            } else {
+                $ctrl.config.beAble[i].able = true;
             }
         }
-        $ctrl.config.selectedIndex=numb;
+        $ctrl.config.selectedIndex = numb;
     };
 
+    $ctrl.habilitar = function (categoria) {
+        if (categoria.porcentaje > 50) {
 
+        } else {
+            alert("Debe Completar al menos 50% de su categoria");
+            categoria.wanted = false;
+        }
+    };
+
+    $ctrl.dialogoEdit = function () {
+        alert("Ventana modal para editar nombre y descripcion");
+    };
 //    *****AUTOCOMPLETE***
     $ctrl.simulateQuery = true;
     $ctrl.isDisabled = false;
